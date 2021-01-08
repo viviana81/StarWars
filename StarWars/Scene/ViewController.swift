bef9360d80ac22eac21efaf3426cddec11f80193
Viviana <vivianacapolvenere@gmail.com>
2021-01-08 16:01:14 +0100

    first commit setup

---
+++ StarWars/Scene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Viviana Capolvenere on 08/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
