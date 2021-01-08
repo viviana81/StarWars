201402802fddc5cdd4e7de510219290995d92857
Viviana <vivianacapolvenere@gmail.com>
2021-01-08 16:19:10 +0100

    added localizable

---
+++ StarWars/Protocol/Localizable.swift
@@ -0,0 +1,54 @@
+//
+//  Localizable.swift
+//  StarWars
+//
+//  Created by Viviana Capolvenere on 08/01/21.
+//
+
+import UIKit
+
+protocol Localizable {
+    var localized: String { get }
+}
+
+protocol XIBLocalizable {
+    var xibLocKey: String? { get set }
+}
+
+extension UILabel: XIBLocalizable {
+    @IBInspectable var xibLocKey: String? {
+        get { return nil }
+        set(key) {
+            text = key?.localized
+        }
+    }
+}
+
+extension UIButton: XIBLocalizable {
+    @IBInspectable var xibLocKey: String? {
+        get { return nil }
+        set(key) {
+            setTitle(key?.localized, for: .normal)
+        }
+    }
+}
+
+extension UITextField: XIBLocalizable {
+    @IBInspectable var xibLocKey: String? {
+        get { return nil }
+        set(key) {
+            placeholder = key?.localized
+        }
+    }
+}
+
+extension String: Localizable {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    
+    var urlEscaped: String {
+        return addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+    }
+}
+
